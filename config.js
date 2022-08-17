7129efce7a488d36753a420032e152510cb82fe7
Thanishkaa004 <thanishkaas07@gmail.com>
2022-08-17 00:36:08 -0700

    Added repository

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyBkq9FMtmjvS0Nie9MxaXUQfjKrV34IUXg",
+    authDomain: "project-67-e1cb8.firebaseapp.com",
+    databaseURL: "https://project-67-e1cb8-default-rtdb.firebaseio.com",
+    projectId: "project-67-e1cb8",
+    storageBucket: "project-67-e1cb8.appspot.com",
+    messagingSenderId: "584425085602",
+    appId: "1:584425085602:web:02cf33f470fbec7d5ed5fb"
 
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
